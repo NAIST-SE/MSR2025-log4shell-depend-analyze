eae11fd18a3f46915d2c8b5ce7049e84ba45ced1
Hietan <yamasaki.kazuma@hietan.com>
2025-03-21 16:00:30 +0900

    add: change Weaver API to Neo4j API

--- src/msr2025/A_Data_Preparation_and_Extraction/main.py
+++ src/msr2025/A_Data_Preparation_and_Extraction/main.py
@@ -1,12 +1,24 @@
-from .utils import run_cypher, run_task, save_result
+import json
+import os
+
+from .utils import run_task
+from .neo4jclient import Neo4jClient
+from dotenv import load_dotenv
 
 SEMVER_REGEX = "'^\\\\d+\\\\.\\\\d+\\\\.\\\\d+$'"
 
 def main():
+    # Setup Neo4j Client
+    load_dotenv()
+    uri = os.getenv("NEO4J_URI")
+    username = os.getenv("NEO4J_USERNAME")
+    password = os.getenv("NEO4J_PASSWORD")
+    client = Neo4jClient(uri, username, password)
+
     # Assign the 'Artifact_log4j' label to the Artifact of 'log4j-core'
     run_task(
         label="Assign the 'Artifact_log4j' label to the Artifact of 'log4j-core'",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(a:Artifact)',
             clause_where='a.id="org.apache.logging.log4j:log4j-core"',
             clause_set='a:Artifact_log4j',
@@ -16,7 +28,7 @@ def main():
     # Assign the 'Release_log4j' label to the Releases of 'log4j-core'
     run_task(
         label="Assign the 'Release_log4j' label to the Releases of 'log4j-core'",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(:Artifact_log4j) - [:relationship_AR] -> (r:Release)',
             clause_set='r:Release_log4j'
         )
@@ -25,7 +37,7 @@ def main():
     # Assign the 'Release_depend' label to the Releases that depend on 'log4j-core'
     run_task(
         label="Assign the 'Release_depend' label to the Releases that depend on 'log4j-core'",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(r:Release) - [:dependency] -> (a:Artifact_log4j)',
             clause_set='r:Release_depend'
         )
@@ -34,7 +46,7 @@ def main():
     # Assign the 'Artifact_depend' label to the Artifacts that depend on 'log4j-core'
     run_task(
         label="Assign the 'Artifact_depend' label to the Artifacts that depend on 'log4j-core'",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(a:Artifact) - [:relationship_AR] -> (:Release_depend)',
             clause_set='a:Artifact_depend'
         )
@@ -44,7 +56,7 @@ def main():
     # that have the 'Release_log4j' label and follow semantic versioning.
     run_task(
         label="Assign the 'Release_log4j_SemVer' label to the Releases that have the 'Release_log4j' label and follow semantic versioning",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(r:Release_log4j)',
             clause_where=f'r.version =~ {SEMVER_REGEX}',
             clause_set='r:Release_log4j_SemVer'
@@ -55,7 +67,7 @@ def main():
     # that follow semantic versioning and whose dependent log4j package versions also follow semantic versioning.
     run_task(
         label="Assign the 'Release_depend_SemVer' label to the Releases that follow semantic versioning and whose dependent log4j package versions also follow semantic versioning",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(r:Release_depend) - [d:dependency] -> (a:Artifact_log4j)',
             clause_where=f'r.version =~ {SEMVER_REGEX} AND d.targetVersion =~ {SEMVER_REGEX}',
             clause_set='r:Release_depend_SemVer'
@@ -65,7 +77,7 @@ def main():
     # Assign the 'artifactId' property to 'Release_depend_SemVer' nodes
     run_task(
         label="Assign the 'artifactId' property to 'Release_depend_SemVer' nodes",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(a:Artifact_depend) - [d:relationship_AR] -> (r:Release_depend_SemVer)',
             clause_set='r.artifactId = a.id'
         )
@@ -74,7 +86,7 @@ def main():
     # Assign the 'targetVersion' property to 'Release_depend_SemVer' nodes
     run_task(
         label="Assign the 'targetVersion' property to 'Release_depend_SemVer' nodes",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(r:Release_depend_SemVer) - [d:dependency] -> (a:Artifact_log4j)',
             clause_set='r.targetVersion = d.targetVersion'
         )
@@ -83,7 +95,7 @@ def main():
     # Assign the 'targetTimestamp' property to 'Release_depend_SemVer' nodes
     run_task(
         label="Assign the 'targetTimestamp' property to 'Release_depend_SemVer' nodes",
-        task=lambda: run_cypher(
+        task=lambda: client.run_query_with_clauses(
             clause_match='(rd:Release_depend_SemVer) - [:dependency] -> (:Artifact_log4j) - [:relationship_AR] -> (rl:Release_log4j_SemVer)',
             clause_where='rd.targetVersion = rl.version',
             clause_set='rd.targetTimestamp = rl.timestamp'
@@ -91,7 +103,31 @@ def main():
     )
 
     # Save Result
-    run_task(label="Save Result", task=save_result)
+    result = {}
+    run_task(
+        label="Save Result",
+        task=lambda: client.extract_data(
+            query="\
+                MATCH (r:Release_depend_SemVer) \
+                WITH \
+                  r, \
+                  split(r.version, ').') AS parts \
+                WITH \
+                  r.artifactId AS artifactId, \
+                  r.version AS lv, \
+                  r.timestamp AS lt, \
+                  r.targetVersion AS rv, \
+                  r.targetTimestamp AS rt, \
+                  toInteger(parts[0]) AS major, \
+                  toInteger(parts[1]) AS minor, \
+                  toInteger(parts[2]) AS patch \
+                ORDER BY artifactId, major, minor, patch \
+                WITH artifactId, collect({lv:lv, lt:lt, rv:rv, rt:rt}) as version \
+                RETURN artifactId, version",
+            path='./output/result.json'
+    ))
+
+    client.close()
 
 
 if __name__ == '__main__':
--- src/msr2025/A_Data_Preparation_and_Extraction/utils.py
+++ src/msr2025/A_Data_Preparation_and_Extraction/utils.py
@@ -1,53 +1,10 @@
 import json
 import os
-from typing import Optional
-from urllib.parse import urljoin
-import requests
-from dotenv import load_dotenv
-from requests import Response
 import itertools
 import threading
 import time
 import sys
-
-
-load_dotenv()
-
-def call_api(path: str, params: dict) -> dict:
-    base_url = os.getenv('WEAVER_REST_API_BASE_URL')
-    if not base_url:
-        raise ValueError("Environment variable 'WEAVER_REST_API_BASE_URL' is not set")
-
-    url: str = urljoin(base_url.rstrip('/') + '/', path.lstrip('/'))
-
-    response: Response = requests.post(url, json=params)
-    response.raise_for_status()
-
-    return response.json()
-
-
-def run_cypher(
-        clause_match: Optional[str] = None,
-        clause_where: Optional[str] = None,
-        clause_set: Optional[str] = None,
-        clause_create: Optional[str] = None,
-        clause_return: Optional[str] = None,
-) -> dict:
-
-    queries: dict[str, Optional[str]] = {
-        'MATCH': clause_match,
-        'WHERE': clause_where,
-        'SET': clause_set,
-        'CREATE': clause_create,
-        'RETURN': clause_return,
-    }
-
-    query: str = " ".join(f"{k} {v}" for k, v in queries.items() if v is not None)
-
-    return call_api(path='/cypher', params={
-        'query': query,
-        'addedValues': []
-    })
+from neo4j import GraphDatabase
 
 
 def spinner(label: str, done_event: threading.Event):
@@ -75,31 +32,6 @@ def run_task(label: str, task):
 def save_json(data, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'w') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=2)
 
 
-def save_result():
-    query: str = "\
-        MATCH (r:Release_depend_SemVer) \
-        WITH \
-          r, \
-          split(r.version, ').') AS parts \
-        WITH \
-          r.artifactId AS artifactId, \
-          r.version AS lv, \
-          r.timestamp AS lt, \
-          r.targetVersion AS rv, \
-          r.targetTimestamp AS rt, \
-          toInteger(parts[0]) AS major, \
-          toInteger(parts[1]) AS minor, \
-          toInteger(parts[2]) AS patch \
-        ORDER BY artifactId, major, minor, patch \
-        WITH artifactId, lv, lt, rv, rt \
-        RETURN artifactId, collect({lv: lv, lt: lt, rv: rv, rt: rt}) as versions"
-
-    result: dict =  call_api(path='/cypher', params={
-        'query': query,
-        'addedValues': []
-    })
-
-    save_json(result, 'output/result_all.json')
